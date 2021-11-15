582c260a4e6cf4af75d345089e3e169047a06551
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2021-11-15 13:57:55 +0700

    finsih use countup.js

--- StudentManagement/Controllers/DashboardController.cs
+++ StudentManagement/Controllers/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Data;
+using StudentManagement.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +11,20 @@ namespace StudentManagement.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly ApplicationDbContext dbcontext;
+        private readonly UserManager<User> userManager;
+
+        public DashboardController(ApplicationDbContext dbcontext, UserManager<User> userManager)
+        {
+            this.dbcontext = dbcontext;
+            this.userManager = userManager;
+        }
         public IActionResult Index()
         {
+            ViewBag.TotalUsers = this.dbcontext.Users.Count();
+            ViewBag.TotalSubjects = this.dbcontext.Subjects.Count();
+            ViewBag.TotalProjects = this.dbcontext.Projects.Count();
+            ViewBag.TotalRooms = this.dbcontext.Rooms.Count();
             return View();
         }
     }
