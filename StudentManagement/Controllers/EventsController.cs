bfd871d591c0c132ca9da441983caa955af26df1
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2022-03-30 23:18:36 +0700

    Finish Edit and Delete action

--- StudentManagement/Controllers/EventsController.cs
+++ StudentManagement/Controllers/EventsController.cs
@@ -15,8 +15,6 @@ namespace StudentManagement.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Consumes("application/json")]
-    [Produces("application/json")]
     public class EventsController : Controller
     {
         private readonly ApplicationDbContext dbcontext;
@@ -43,17 +41,22 @@ namespace StudentManagement.Controllers
         }
 
         [HttpDelete]
-        public async Task<JsonResult> DeleteEvent(Guid eventId)
+        public async Task<JsonResult> DeleteEvent(string eventId)
         {
             var status = false;
-            var v = await this.dbcontext.Events.Where(x => x.Id == eventId).SingleOrDefaultAsync();
-            if (v != null)
+            var currEvent = await this.dbcontext.Events.Where(x => x.Id == Guid.Parse(eventId)).SingleOrDefaultAsync();
+            if (currEvent != null)
             {
-                this.dbcontext.Remove(v);
+                this.dbcontext.Remove(currEvent);
                 await this.dbcontext.SaveChangesAsync();
                 status = true;
+
+                return Json(new { Data = new { status = status } });
             }
-            return Json(new { Data = new { status = status } });
+            else
+            {
+                return Json(new { Data = new { status = status } });
+            }
         }
 
         [HttpPost]
@@ -61,35 +64,33 @@ namespace StudentManagement.Controllers
         {
             var status = false;
 
-            //if (e.Id != null)
-            //{
-            //    var currEvent = await this.dbcontext.Events.Where(x => x.Id == e.Id).SingleOrDefaultAsync();
-
-            //    if (currEvent != null)
-            //    {
-            //        currEvent.Title = e.Title;
-            //        currEvent.Description = e.Description;
-            //        currEvent.StartTime = e.StartTime;
-            //        currEvent.EndTime = e.EndTime;
-            //        currEvent.ThemeColor = e.ThemeColor;
-            //    }
-            //}
-            //else
-            //{
-               
-            //}
-
-            var user = await this.userManager.GetUserAsync(this.HttpContext.User);
-            var newEvent = new Event
+            if (e.Id != null)
             {
-                Title = e.Title,
-                Description = e.Description,
-                StartTime = e.StartTime,
-                EndTime = e.EndTime,
-                ThemeColor = e.ThemeColor,
-                User = user
-            };
-            this.dbcontext.Events.Add(newEvent);
+                var currEvent = await this.dbcontext.Events.Where(x => x.Id == Guid.Parse(e.Id)).SingleOrDefaultAsync();
+
+                if (currEvent != null)
+                {
+                    currEvent.Title = e.Title;
+                    currEvent.Description = e.Description;
+                    currEvent.StartTime = e.StartTime;
+                    currEvent.EndTime = e.EndTime;
+                    currEvent.ThemeColor = e.ThemeColor;
+                }
+            }
+            else
+            {
+                var user = await this.userManager.GetUserAsync(this.HttpContext.User);
+                var newEvent = new Event
+                {
+                    Title = e.Title,
+                    Description = e.Description,
+                    StartTime = e.StartTime,
+                    EndTime = e.EndTime,
+                    ThemeColor = e.ThemeColor,
+                    User = user
+                };
+                this.dbcontext.Events.Add(newEvent);
+            }
 
             await this.dbcontext.SaveChangesAsync();
             status = true;
